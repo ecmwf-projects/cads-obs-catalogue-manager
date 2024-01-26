2e9d00e38c3537dff3202cd5468b95c3371a8c31
aperezpredictia <52523290+aperezpredictia@users.noreply.github.com>
2024-01-26 12:04:31 +0100

    Merge pull request #2 from ecmwf-projects/fix-copy-dataset
    Fix copy dataset

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -5,3 +5,10 @@ def test_s3_client_interface(test_s3_client):
         "cds2-obs-dev-insitu-observations-woudc-ozone-total-column-and-p"
     )
     assert actual_bucket_name == expected_bucket_name
+    # Test for removing the last hyphen
+    dataset_name = "iinsitu-observations-woudc-ozone-total-column-and-profiles"
+    actual_bucket_name = test_s3_client.get_bucket_name(dataset_name)
+    expected_bucket_name = (
+        "cds2-obs-dev-iinsitu-observations-woudc-ozone-total-column-and"
+    )
+    assert actual_bucket_name == expected_bucket_name
