70b920e0da7acb1b37c8024879554eaa24f30a93
garciam <garciam@predictia.es>
2024-07-04 17:27:29 +0200

    rename the docstring too

--- cdsobs/cli/_validate.py
+++ cdsobs/cli/_validate.py
@@ -9,7 +9,7 @@ from cdsobs.cli._utils import CliException
 def validate_service_definition(
     service_definition: str = typer.Argument(..., help="Path to JSON file")
 ):
-    """Validate a service definition JSON file."""
+    """Validate a service definition YAML file."""
     if not Path(service_definition).exists():
         raise CliException("File not found")
     cdsobs.service_definition.api.validate_service_definition(service_definition)
