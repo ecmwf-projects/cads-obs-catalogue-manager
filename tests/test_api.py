346ef6ed030b28160ce479a5bbbed0ffd61b98c7
garciam <garciam@predictia.es>
2024-08-27 10:19:49 +0200

    check missing variable script
    Saves the results in a table

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,12 +5,8 @@ import pytest
 import sqlalchemy as sa
 
 from cdsobs.api import run_ingestion_pipeline, run_make_cdm
-from cdsobs.cdm.api import open_netcdf
-from cdsobs.cdm.lite import auxiliary_variable_names
-from cdsobs.ingestion.core import get_aux_vars_from_service_definition
 from cdsobs.observation_catalogue.models import Catalogue
 from cdsobs.service_definition.api import get_service_definition
-from cdsobs.storage import S3Client
 from cdsobs.utils.logutils import get_logger
 from tests.utils import get_test_years
 
@@ -90,39 +86,6 @@ def test_run_ingestion_pipeline(
         .where(Catalogue.dataset == dataset_name)
     )
     assert counter > 0
-    # Check variables
-    asset = test_session.scalar(
-        sa.select(Catalogue.asset).where(Catalogue.dataset == dataset_name)
-    )
-    s3client = S3Client.from_config(test_config.s3config)
-    asset_filename = asset.split("/")[1]
-    asset_local_path = Path(tmp_path, asset_filename)
-    s3client.download_file(
-        s3client.get_bucket_name(dataset_name), asset_filename, asset_local_path
-    )
-    dataset = open_netcdf(asset_local_path, decode_variables=True)
-    variables_in_file = set(
-        dataset.columns.tolist() + dataset.observed_variable.unique().tolist()
-    )
-    aux_variables = get_aux_vars_from_service_definition(service_definition, source)
-    expected_variables = set(service_definition.sources[source].descriptions) - set(
-        aux_variables
-    )
-    for v in [
-        "observed_variable",
-        "observation_value",
-        "units",
-    ] + auxiliary_variable_names:
-        if v in variables_in_file:
-            expected_variables.add(v)
-    logger.info(
-        f"{dataset_name=} {source=}: {variables_in_file - expected_variables} "
-        f"are in file but not in the descriptions"
-    )
-    logger.info(
-        f"{dataset_name=} {source=}: {expected_variables - variables_in_file} "
-        f"are not in file but are in the descriptions"
-    )
 
 
 def test_make_cdm(test_config, tmp_path, caplog):
