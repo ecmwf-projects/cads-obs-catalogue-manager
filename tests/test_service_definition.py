437d1752fa503150fe5dbe1f78442271690bb22f
garciam <garciam@predictia.es>
2024-11-12 11:20:40 +0100

    fix test

--- tests/test_service_definition.py
+++ tests/test_service_definition.py
@@ -8,12 +8,12 @@ from cdsobs.service_definition.api import validate_service_definition
 def test_new_service_definition_valid(caplog, test_config):
     SERVICE_DEFINITION_YML = Path(
         cdsobs_path,
-        "data/insitu-observations-ndacc/service_definition.yml",
+        "data/insitu-observations-igra-baseline-network/service_definition.yml",
     )
     with caplog.at_level(logging.ERROR):
         validate_service_definition(
             str(SERVICE_DEFINITION_YML),
             test_config.cdm_tables_location,
-            validate_cdm=False,
+            validate_cdm=True,
         )
     print(caplog.text)
