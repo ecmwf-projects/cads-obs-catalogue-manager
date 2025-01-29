66e4d1fcefbd633544098fdcd64236ac53f69af9
garciam <garciam@predictia.es>
2025-01-29 09:56:39 +0100

    update with NDACC

--- tests/scripts/make_production.sh
+++ tests/scripts/make_production.sh
@@ -18,12 +18,12 @@ dataset="insitu-observations-igra-baseline-network"
 
 cadsobs make-production -d ${dataset} \
   -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
-  -c cdsobs_config.yml --start-year 1978 --end-year 2020 \
+  --start-year 1978 --end-year 2024 \
   --source IGRA  >& make_production_igra_igra.log
 
 cadsobs make-production -d ${dataset} \
   -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
-  -c cdsobs_config.yml --start-year 1978 --end-year 2020 \
+  --start-year 1978 --end-year 2024 \
   --source IGRA_H  >& make_production_woudc_igra_igra-h.log
 
 # GRUAN
@@ -40,14 +40,18 @@ dataset="insitu-observations-gnss"
 
 cadsobs make-production -d ${dataset} \
   -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
-  -c cdsobs_config.yml --start-year 1996 --end-year 2014   \
+  -c cdsobs_config.yml --start-year 1996 --end-year 2024   \
   --source EPN  >& make_production_gnss_epn.log
 
 cadsobs make-production -d ${dataset} \
   -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
-  -c cdsobs_config.yml --start-year 1996 --end-year 2014   \
+  -c cdsobs_config.yml --start-year 1996 --end-year 2024   \
   --source IGS  >& make_production_gnss_igs.log
 
+cadsobs make-production -d ${dataset} \
+  -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  -c cdsobs_config.yml --start-year 1996 --end-year 2024   \
+  --source IGS_R3  >& make_production_gnss_igs_r3.log
 # USCRN
 
 dataset="insitu-observations-near-surface-temperature-us-climate-reference-network"
@@ -78,3 +82,34 @@ dataset="insitu-comprehensive-upper-air-observation-network"
 cadsobs make-production -d ${dataset} \
   -s cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
   -c cdsobs_config.yml --start-year 1924 --end-year 2023 >& make_production_cuon.log
+
+# NDACC
+
+dataset="insitu-observations-ndacc"
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1983 --end-year 2024 --source Brewer_O3 >& make_production_ndacc_Brewer_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1983 --end-year 2024 --source CH4 >& make_production_ndacc_CH4.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1985 --end-year 2024 --source CO >& make_production_ndacc_CO.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1963 --end-year 2024 --source Dobson_O3 >& make_production_ndacc_Dobson_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1985 --end-year 2024 --source Ftir_profile_O3 >& make_production_ndacc_Ftir_profile_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1985 --end-year 2024 --source Lidar_profile_O3 >& make_production_ndacc_Lidar_profile_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1985 --end-year 2024 --source Mwr_profile_O3 >& make_production_ndacc_Mwr_profile_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1966 --end-year 2024 --source OzoneSonde_O3 >& make_production_ndacc_OzoneSonde_O3.log
+cadsobs make-production -d ${dataset} \
+  -s ../cads-obs-catalogue-manager/cdsobs/data/${dataset}/service_definition.yml \
+  --start-year 1985 --end-year 2024 --source Uvvis_profile_O3 >& make_production_ndacc_Uvvis_profile_O3.log
