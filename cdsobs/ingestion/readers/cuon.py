c54f06d811f5e38261dcfdf193bc6d31ace5ebd0
garciam <garciam@predictia.es>
2025-06-06 16:49:52 +0200

    merge adjustments in the reader

--- cdsobs/ingestion/readers/cuon.py
+++ cdsobs/ingestion/readers/cuon.py
@@ -430,6 +430,29 @@ def get_denormalized_table_file(
     denormalized_table_file["quality_flag"] = 2
     denormalized_table_file["homogenisation_method"] = 14
     denormalized_table_file["report_meaning_of_timestamp"] = 1
+    # Merge homogenisation adjustments
+    homogenisation_adjustment = denormalized_table_file["homogenisation_adjustment"]
+    observed_variable = denormalized_table_file["observed_variable"]
+    # Humidity
+    humidity_adjustment = denormalized_table_file["humidity_bias_estimate"]
+    humidity_variables = [
+        "relative_humidity",
+        "specific_humidity",
+        "dew_point_depression",
+        "air_dewpoint",
+    ]
+    mask = numpy.isin(observed_variable, humidity_variables)
+    homogenisation_adjustment.loc[mask] = humidity_adjustment[mask]
+    # Wind
+    wind_adjustment = denormalized_table_file["wind_bias_estimate"]
+    wind_variables = [
+        "wind_speed",
+        "eastward_wind_speed",
+        "northward_wind_speed",
+        "wind_from_direction",
+    ]
+    mask = numpy.isin(observed_variable, wind_variables)
+    homogenisation_adjustment.loc[mask] = wind_adjustment[mask]
     return denormalized_table_file
 
 
