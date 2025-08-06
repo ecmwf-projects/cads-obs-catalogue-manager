9edcd9e6d3ac1c2140e36d71de0106cbd4041ecc
garciam <garciam@predictia.es>
2025-08-06 14:47:30 +0200

    fix test

--- cdsobs/ingestion/notify.py
+++ cdsobs/ingestion/notify.py
@@ -6,8 +6,8 @@ import requests
 
 
 def notify_to_slack(message: str, post_function: Callable = requests.post):
-    webhook_url = os.environ["CADSOBS_SLACK_WEBHOOK"]
-    channel = os.environ["CADSOBS_SLACK_CHANNEL"]
+    webhook_url = os.getenv("CADSOBS_SLACK_WEBHOOK")
+    channel = os.getenv("CADSOBS_SLACK_CHANNEL")
     slack_data = {"username": "cadsobs-bot", "channel": channel, "text": message}
     response = post_function(
         webhook_url,
